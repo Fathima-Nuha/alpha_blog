6010550a1cc6012b3914ce4fd30d6364c84b5ba3
Nuha <fathimanuha0002@gmail.com>
2024-02-01 16:32:41 +0530

    added password digest to user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+	before_save {self.email = email.downcase}
+	has_secure_password
 	has_many :artiicles
 	validates :username, uniqueness: { case_sensitive: true },presence: true,length: { minimum: 3 }
 	validates :email, uniqueness: true,presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "Invalid email format" }
